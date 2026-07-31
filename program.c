ca7d7f08ce922e2896fc30f71c0cfc9a673d80c0
Aapka Naam <aapka-email@gmail.com>
2026-07-31 20:20:54 +0530

    Added C program

---
+++ program.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, sum;
+    printf("pehla number likho: ");
+    scanf("%d", &num1);
+
+    printf("dusra number likho: ");
+    scanf("%d", &num2);
+
+    sum = num1 + num2;
+
+   printf("total = %d\n", sum);
+
+   return 0;
+}
+
